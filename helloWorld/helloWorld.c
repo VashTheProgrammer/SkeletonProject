87177b4795015f97fc765a64de080ba14cba6cbc
GitFoxy <lucapetralito03@gmail.com>
2022-08-29 01:04:59 +0200

    cBuf test
    Playing with the circular buffer

--- helloWorld/helloWorld.c
+++ helloWorld/helloWorld.c
@@ -4,14 +4,56 @@
 #define TASK1_PERIOD 1 * 1000 * 1000
 #define TASK2_PERIOD 1 * 1000 * 1000 * 10
 
+#define CBUF_HFDATA_SIZE 8192
+
+struct Element
+{
+	int value;
+	uint64_t timestamp;
+};
+
+
+
+circ_gbuf_t cBufHFData;
+CIRC_GBUF_DEF(struct Element, cBufHFData, CBUF_HFDATA_SIZE);
+
+
+
 int test(uint8_t pid)
 {
     printf("test\n");
+
+    struct Element element;
+
+    element.timestamp = getMicrosecTime();
+
+    element.value = rand();
+
+    if (CIRC_GBUF_PUSH(cBufHFData, &element) == 0)
+		{
+			printf("Push: %d at: %d\n", element.value, element.timestamp);
+		}
+
     return 0;
 }
 
 int logger_test(uint8_t pid)
 {
+    struct Element element;
+
+    element.timestamp = 69;
+
+    element.value = 69;
+
+    printf("Pop out: %d at: %d\n", element.value, element.timestamp);
+
+    if (CIRC_GBUF_POP(cBufHFData, &element) == 0)
+		{
+            printf("Pop in: %d at: %d\n", element.value, element.timestamp);
+        }
+
+    
+    
     log_trace("logger trace test");
     log_debug("logger debug test");
     log_info("logger info test");
@@ -25,6 +67,8 @@ int logger_test(uint8_t pid)
 int main()
 {
     log_info("Skeleton %s starting...\n", SKELETON_VERSION);
+
+    CIRC_GBUF_FLUSH(cBufHFData);
 
     PROCESS_ATTACH(SCHEDULER_INSTANCE, TASK1_PERIOD, test);
     PROCESS_ATTACH(SCHEDULER_INSTANCE, TASK2_PERIOD, logger_test);
